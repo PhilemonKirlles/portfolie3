dc0c4ffd7a7a65adf7560bfb730e6c255c5f8a04
Philemon Kirlles <philemon.kirlles@gmail.com>
2022-09-24 21:39:57 -0400

    pages index.js

---
+++ philemon-kirlles-portfolio/src/components/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+  
+const Contact = () => {
+  return (
+    <div>
+      <h1>Mail Me at philemon.kirlles@gmail.com</h1>
+    </div>
+  );
+};
+  
+export default Contact;
--- philemon-kirlles-portfolio/src/components/pages/index.js
+++ philemon-kirlles-portfolio/src/components/pages/index.js
@@ -10,30 +10,3 @@ const Home = () => {
 };
 export default Home;
 
-import { Nav, NavLink, NavMenu } from "./NavbarElements";
-
-const Navbar = () => {
-  return (
-    <>
-      <Nav>
-        <NavMenu>
-          <NavLink to="/about" activeStyle>
-            About
-          </NavLink>
-          <NavLink to="/contact" activeStyle>
-            Contact
-          </NavLink>
-          <NavLink to="/blog" activeStyle>
-            Blog
-          </NavLink>
-          <NavLink to="/sign-up" activeStyle>
-            Sign Up
-          </NavLink>
-        </NavMenu>
-      </Nav>
-    </>
-  );
-};
-
-export default Navbar;
-
